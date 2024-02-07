48b6d4988ed2ce08c4f9e7a2c7e9e81e5ced483e
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-07 10:34:35 +0500

    Add new assets and update Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { RiMenu3Line } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+import "./navbar.css";
 
 const Navbar = () => {
-	return <div className="gpt__navbar">
-		<div className="gpt__navbar-links">
-			
+	return (
+		<div className="gpt__navbar">
+			<div className="gpt__navbar-links">
+				<div className="gpt__nvabar-links_logo">
+					<img src={logo} alt="logo" />
+				</div>
+				<div className="gpt__navbar-links_container">
+					<p>
+						<a href="#home">Home</a>
+					</p>
+					<p>
+						<a href="#wgpt3">What is GPT3?</a>
+					</p>
+					<p>
+						<a href="#possibility">Open AI</a>
+					</p>
+					<p>
+						<a href="#feature">Case Studies</a>
+					</p>
+					<p>
+						<a href="#blog">Library</a>
+					</p>
+				</div>
+			</div>
 		</div>
-	</div>;
+	);
 };
 
 export default Navbar;
