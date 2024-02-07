3b9eea674a4d640bdc9d9cc48b62dfb6e75bed6d
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-07 13:58:08 +0500

    feat: Add Brands Component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { Google, Slack, Atlassian, Dropbox, Shopify } from "./imports";
 import "./brand.css";
 
 const Brand = () => {
-	return <div>Brand</div>;
+	return (
+		<div className="gpt__brand section__padding">
+			<div>
+				<img src={Google} alt="google" />
+			</div>
+			<div>
+				<img src={Slack} alt="Slack" />
+			</div>
+			<div>
+				<img src={Atlassian} alt="Atlassian" />
+			</div>
+			<div>
+				<img src={Dropbox} alt="Dropbox" />
+			</div>
+			<div>
+				<img src={Shopify} alt="Shopify" />
+			</div>
+		</div>
+	);
 };
 
 export default Brand;
